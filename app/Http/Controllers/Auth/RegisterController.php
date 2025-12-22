881fe0fe677d972a84d3f0824dbdecb668fa88a9
atuning120 <90119017+atuning120@users.noreply.github.com>
2025-12-22 16:51:35 -0300

    verificar datos, subir a la base de datos

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Symfony\Component\VarDumper\Dumper\ContextProvider\RequestContextProvider;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Symfony\Contracts\Service\Attribute\Required;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\RequestContextProvider;
 
 class RegisterController extends Controller
 {
@@ -19,13 +22,24 @@ class RegisterController extends Controller
     {
         //dd($request->get('username'));
 
+        $request->request->add(['username' => Str::slug($request->username)]);
 
         //validaciones de campos
         $request->validate([
             'name' => 'required|string|max:30|min:3',
             'username' => 'required|min:3|unique:users|max:30',
             'email' => 'required|email|unique:users|max:60',
-            'password' => 'required|string|confirmed|min:6',
+            'password' => 'required|confirmed|min:6',
         ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' =>$request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        //redireccionar
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,4 +11,7 @@ Route::get('/', function () {
 Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
 
 Route::post('/crear-cuenta', [RegisterController::class, 'store']);
+
+
+Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
 
